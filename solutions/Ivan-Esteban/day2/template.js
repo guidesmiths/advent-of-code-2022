81c16f80838194f8422e06aeed6696062df24806
Ivan Esteban <ivan.esteban@guidesmiths.com>
2022-12-02 15:19:57 +0100

    ivan solutions

---
+++ solutions/Ivan-Esteban/day2/template.js
@@ -0,0 +1,16 @@
+// Personal data set
+const inputCase = ``;
+
+// Provided test case
+const testCase = ``;
+
+// Solution
+const resolve = (input) => console.log('WRITE YOUR SOLUTION');
+
+const test = resolve(testCase);
+
+console.assert(test === 'PUT HERE TEST SOLUTION', 'Test case failed');
+
+const output = resolve(inputCase);
+
+console.log(`The solution is ${output}`);
